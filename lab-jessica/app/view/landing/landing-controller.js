46d99a3ecbbb437627ccda87e21079c305acc80b
Jessica Soltero <jmichellevs@gmail.com>
2017-03-06 15:13:29 -0800

    Did some styling for landing page

--- lab-jessica/app/view/landing/landing-controller.js
+++ lab-jessica/app/view/landing/landing-controller.js
@@ -4,7 +4,7 @@ require('./_landing.scss');
 
 module.exports = ['$log', '$location', '$rootScope', 'authService', LandingController];
 
-function LandingController($log, $location, authService) {
+function LandingController($log, $location) {
   let url = $location.url();
   this.showSignup = url === '/join#signup' || url === '/join';
 }
